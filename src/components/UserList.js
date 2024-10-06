aca6a353b025ac9bc24837744bd66759f9e98115
Sagar Gupta <55579984+sagargupta121102@users.noreply.github.com>
2024-10-07 00:04:18 +0530

    Error Resolved

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './Spinner.css'; // Add CSS for Spinner
 
 const UserList = () => {
